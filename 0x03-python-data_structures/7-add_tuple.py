3feea29948a3adec15014b3a38cac96cd30eac09
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-05-04 22:17:15 +0300

    rewrote the whole code

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    sumation = ((tuple_a[0] if len(tuple_a) > 0 else 0) + (tuple_b[0] if len(tuple_b) > 0 else 0),
-           (tuple_a[1] if len(tuple_a) > 1 else 0) + (tuple_b[1] if len(tuple_b) > 1 else 0))
-    return sumation
+    a = len(tuple_a)
+    b = len(tuple_b)
+    sums = ((tuple_a[0] if a > 0 else 0) + (tuple_b[0] if b > 0 else 0),
+            (tuple_a[1] if a > 1 else 0) + (tuple_b[1] if b > 1 else 0))
+    return sums
