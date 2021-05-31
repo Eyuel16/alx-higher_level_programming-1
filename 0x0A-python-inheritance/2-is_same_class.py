63b0a1b2dbed977cb2a99d96195571175449f1d0
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-05-31 23:29:06 +0300

    syntax correction

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -8,4 +8,4 @@ and methods
 
 def is_same_class(obj, a_class):
     """ is the objec is exactly an instance of the specified class """
-    return isinstance(obj, a_class)
+    return type(obj) == a_class
