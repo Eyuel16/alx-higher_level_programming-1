f34e7c5b85225fead7a431779985fae231ced29c
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-05-31 23:39:14 +0300

    corrected the comment

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 """
 Module 10-square
-Contains parent class BaseGeometry
-with public instance method area and integer_validation
-Contains subclass Rectangle
-with instantiation of private attributes width and height, validated by parent,
-extends parent's area method and prints with __str__
-Contains subclass Square
-with instantiation of private attribute size, validated by superclass
+Square class
+with size attribute
 """
 
 
