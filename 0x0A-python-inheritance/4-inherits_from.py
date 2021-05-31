65ff3c342821879ef00332f84110b46f7b05a422
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-05-31 23:32:51 +0300

    issubclass mut be a class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,4 +8,4 @@ and methods
 
 def inherits_from(obj, a_class):
     """ is the objec is exactly an instance of the specified class """
-    return issubclass(obj, a_class)
+    return (type(obj) is not a_class and issubclass(type(obj), a_class))
