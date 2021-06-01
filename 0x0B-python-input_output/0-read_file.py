6745568968bd25e6ee7adebd5f9eb5300247189c
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-06-01 23:47:06 +0300

    added endline

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ doesn't specifiy permission
 def read_file(filename=""):
     """ reads a file """
     with open(filename, encoding='utf-8') as my_file:
-        print(my_file.read())
+        print(my_file.read(), end="")
