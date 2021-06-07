343191a904150f20485a75bccb8188435cb86f63
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-06-07 21:31:09 +0300

    a specific square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""
+Module square
+Contains a class of a square
+that inherits from the Rectangle
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ A square that inherits from a rectangle """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ initializes the square """
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    @property
+    def size(self):
+        """ size getter """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ size setter """
+        self.height = value
+        self.width = value
+
+    def __str__(self):
+        """ prints detail of the square """
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
+                                                         self.x,
+                                                         self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        """ updates the class arguments """
+        if args:
+            for key, value in enumerate(args):
+                if key == 0:
+                    self.id = value
+                elif key == 1:
+                    self.size = value
+                elif key == 2:
+                    self.x = value
+                else:
+                    self.y = value
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        """ returns the dictionary representaiton of a Square """
+        dic = {}
+        dic["id"] = self.id
+        dic["size"] = self.size
+        dic["x"] = self.x
+        dic["y"] = self.y
+        return dic
