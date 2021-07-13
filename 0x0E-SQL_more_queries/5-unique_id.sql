db253f23a2f1b6ce4489abc021c5a7cdd10aacf5
Amanuel Sisay <amanuelsisay.as@gmail.com>
2021-07-13 20:17:25 +0000

    creates a table with default value

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -2,7 +2,7 @@
 -- named unique_id
 -- will not fail if it already exists
 
-CREATE TABEL IF NOT EXISTS unique_id (
-       id INT  DEFAULT 1 UNIQUE,
+CREATE TABLE IF NOT EXISTS unique_id (
+       id INT DEFAULT 1 UNIQUE,
        name VARCHAR(256)
 );
