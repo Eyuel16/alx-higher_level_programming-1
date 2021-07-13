f755d1a9f2b9dc5250e5ec785899a60cbf19e6cc
Amanuel Sisay <amanuelsisay.as@gmail.com>
2021-07-13 20:36:19 +0000

    select california cities

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- lists all the cities
+-- of california that can be found in the
+-- database
+
+
+SELECT id, name
+  FROM cities
+ WHERE state_id = (SELECT id FROM states WHERE name = "California") GROUP BY id ORDER BY id ASC;
