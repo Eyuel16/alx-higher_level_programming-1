984835d4c28b87dceb809cf3f3ee4ec7c75076a5
Amanuel Sisay <amanuelsisay.as@gmail.com>
2021-07-13 20:01:36 +0000

    create a table

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,9 @@
--- creates a table
+-- creates the table force_name
+-- data in table: id INT, name VARCHAR(256) t be null
+-- database name will be passed as an argument of the mysql command
+-- if table exists, script should not fail
 
-CREATE TABLE IF NOT EXISTS foce_name (
+CREATE TABLE IF NOT EXISTS force_name (
        id INT,
        name VARCHAR(256) NOT NULL
 );
