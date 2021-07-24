3f12fae6528f3a451f0a1701ff19fcd0185f322f
Amanuel Abiy <amanuelabiy.as@gmail.com>
2021-07-24 17:06:28 +0300

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -4,16 +4,20 @@ Method 9-student
 Contains a class that defines a student
 and a method to retrieve a dictionary representation of the class
 """
-
-
-class Student:
-    """Class for student"""
+class Student():
+    """
+        to_json: retrieves its dictionary representation
+    """
     def __init__(self, first_name, last_name, age):
-        """Constructor."""
+        """
+        Initializes student with full name and age
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """retrieves dictionary"""
-        return self.__dict__.copy()
+        """
+        Returns dictionary description with simple data structure
+        """
+        return self.__dict__
