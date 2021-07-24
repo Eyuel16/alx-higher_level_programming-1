491ddcdecd7f7bbe98a5d5f7dade9de93e96ea32
Amanuel Abiy <amanuelabiy.as@gmail.com>
2021-07-24 17:12:53 +0300

    The pascals triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -7,11 +7,18 @@ integers representing the pascal's triangle of n
 
 
 def pascal_triangle(n):
-    """ prints a pascal triangle with size n """
-    pascal_list = [1]
-    for i in range(n):
-        pascal_list[i] = 1
-        for j in range(n):
-            pascal_list[i][j] = pascal_list[i - 1][j - 1] + pascal_list[i - 1][j]
-    print (pascal_list)
-    return pascal_list
+    """ the Pascal’s triangle """
+    if n <= 0:
+        return []
+    else:
+        list = [[] for i in range(n)]
+        for i in range(n):
+            for j in range(i + 1):
+                if(j < i):
+                    if(j == 0):
+                        list[i].append(1)
+                    else:
+                        list[i].append(list[i - 1][j] + list[i - 1][j - 1])
+                elif(j == i):
+                    list[i].append(1)
+        return list
