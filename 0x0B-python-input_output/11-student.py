5abc4ab2a4da08411e68a939f4742198e9f6ab53
Amanuel Abiy <amanuelabiy.as@gmail.com>
2021-07-24 17:09:21 +0300

    to json and reload

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -3,37 +3,28 @@
 Method 9-student
 Contains a class that defines a student
 and a method to retrieve a dictionary representation of the class
-"""class Student():
-    """
-    Public Attributes:
-        first_name
-        last_name
-        age
-    Public Methods:
-        to_json: retrieves its dictionary representation
-    """
+"""
+class Student:
+    """Class for student"""
     def __init__(self, first_name, last_name, age):
-        """
-        Initializes student with full name and age
-        """
+        """Constructor."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        Returns dictionary description with simple data structure
-        (list, dictionary, dictionary, string)
-        for JSON serialization of an object
-        Return:
-            Only return dict of attrs given to us
-            Return entire dict if no attrs given
-        """
+        """retrieves a dictionary representation of a Student instance"""
         if attrs is None:
             return self.__dict__
-        else:
-            dic = {}
-            for att in attrs:
-                if att in self.__dict__.keys():
-                    dic[att] = self.__dict__[att]
-            return dic
+        dictionary = {}
+        for attr in attrs:
+            try:
+                dictionary[attr] = self.__dict__[attr]
+            except Exception:
+                pass
+        return dictionary
+
+    def reload_from_json(self, json):
+        """loads attributes from json"""
+        for key, value in json.items():
+            self.__dict__[key] = value
