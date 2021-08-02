99c5d545b9e212204ed6f6593b51571aad7937e5
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-08-03 01:23:51 +0300

    print the square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const x = parseInt(process.argv[2], 10);
+if (isNaN(x)) {
+  console.log('Missing size');
+} else {
+    for (let i = 1; i <= x; i += 1) {
+	console.log('x'.repeat(x));
+    }
+}
