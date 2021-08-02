1b181a09d673c98a2f0e22d9ac67462e65c8ee29
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-08-03 01:09:55 +0300

    added curly braces

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const num = parseInt(process.argv[2], 10);
-if(isNaN(num))
+if(isNaN(num)) {
     console.log('Not a number');
-else
+} else {
     console.log(`My number: ${num}`);
+}
