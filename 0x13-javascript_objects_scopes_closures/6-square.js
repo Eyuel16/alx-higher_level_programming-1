20b019b601d5368ba4895d4346cde2d52755fc7a
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-08-03 23:47:39 +0300

    added an empty line

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 const Squares = require('./5-square');
+
 module.exports = class Square extends Squares {
   charPrint (c) {
     if (c === undefined) {
       return this.print();
     } else {
-      for (let col = 0; col < this.width; col += 1) {
+      for (let x = 0; x < this.width; x += 1) {
 	console.log(c.repeat(this.height));
       }
     }
