69717e94827416a689ae39dc843878b8db199f5a
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-08-04 00:36:30 +0300

    dictionary

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-
 const dict = require('./101-data').dict;
-
-let newDic = {};
-
+const newDic = {};
 for (let key in dict) {
   if (newDic[dict[key]] === undefined) {
     newDic[dict[key]] = [];
