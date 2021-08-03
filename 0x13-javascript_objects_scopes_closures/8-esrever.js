2d40aa77165e0b58b9354556ec35453ed1735e7a
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-08-04 00:42:51 +0300

    reverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   const myList = [];
   for (let i = list.length - 1; i >= 0; i--) {
-    const.push(list[i]);
+    myList.push(list[i]);
   }
   return myList;
 };
