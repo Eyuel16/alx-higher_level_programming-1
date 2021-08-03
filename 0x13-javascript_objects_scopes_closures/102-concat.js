d7cbadf5993740b5669a5f28a2cea70550aca8e3
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-08-04 00:30:01 +0300

    concat two files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,4 +6,5 @@ let fileC = process.argv[4];
 const fs = require('fs');
 let textA = fs.readFileSync(fileA, 'utf8');
 let textB = fs.readFileSync(fileB, 'utf8');
+
 fs.writeFileSync(fileC, textA + textB);
