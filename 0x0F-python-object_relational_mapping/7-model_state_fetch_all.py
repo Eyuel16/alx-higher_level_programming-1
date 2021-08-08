c3ed1f06177f563a745ca770e6dfe6c1d5c3e358
Amanuel Abiy <amanuelabiy.as@gmail.com>
2021-08-08 21:36:53 +0300

    Update 7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
 """
-contains the class definition of State and instatance Base
+return all state objects from database via python
+parameters given to script: username, password, database
 """
+
+from sys import argv
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-from sys import argv
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     user = argv[1]
-    password = argv[2]
-    database = argv[3]
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".
-                           format(user, password, database),
-                           pool_pre_ping=True)
-    session = sessionmaker(bind=engine)
-    session_ = session()
-    for row in session_.query(State).order_by(State.id):
-        print("{:d}: {:s}".format(row.id, row.name))
+    passwd = argv[2]
+    db = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(user, passwd, db), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for instance in session.query(State).order_by(State.id):
+        print("{:d}: {:s}".format(instance.id, instance.name))
+
     session.close()
