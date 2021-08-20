854258e0c43cd82c8e425e5e9bc18d9a85c32273
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-08-21 01:54:16 +0300

    get commits list from github

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,6 +12,8 @@ from sys import argv
 if __name__ == '__main__':
     values = {'owner': argv[1],
               'repo': argv[2]}
-    url = "https://api.github.com/repos" + argv[1] + "/" + argv[2] + "/commits"
+    url = "https://api.github.com/repos/" + argv[1] + "/" + argv[2] + "/commits"
     res = requests.get(url)
-    print(res.json().get('sha'))
+    dic = res.json()
+    for item in dic:
+        print("{}: {}".format(item.get('sha'), item.get('commit').get('author').get('name')))
