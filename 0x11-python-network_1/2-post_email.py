f2e2e2e1b0fed485ef720509577e645fc4616fba
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-08-20 23:50:44 +0300

    autopep8

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,7 +12,7 @@ import urllib.parse
 
 
 if __name__ == "__main__":
-    value = {'email' : sys.argv[2]}
+    value = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(value)
     data = data.encode('ascii')
     req = urllib.request.Request(sys.argv[1], data)
