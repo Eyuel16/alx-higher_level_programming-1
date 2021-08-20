3e75812d2cb4661665a9aa6d5173c7692f6deab1
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-08-21 02:11:29 +0300

    fixed syntax error

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,7 +11,7 @@ import requests
 
 if __name__ == '__main__':
     r = requests.get(argv[1])
-    if (r.status_code < 400)
+    if r.status_code < 400:
         print(r.text)
     else:
         print("Error code: {}".format(r.status_code))
