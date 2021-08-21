d6d82257c7a1431dd0422ca81ac02e55e5f0e183
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-08-21 14:14:58 +0300

    fixed logical error

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,8 +5,9 @@ fins the peak of a list
 
 def find_peak(list_of_integers):
     """ a function to return the first peak found """
-    i = 1
-    for i in range(len(list_of_integers)):
+    for i in range(1, len(list_of_integers)):
+        if i + 1 >= len(list_of_integers):
+            return None
         if ((list_of_integers[i] >= list_of_integers[i - 1]) and
             (list_of_integers[i] >= list_of_integers[i + 1])):
             return list_of_integers[i]
