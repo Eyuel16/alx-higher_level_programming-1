03ccc072c7c199ca0ffadd8e87893427068277a8
Amanuel Abiy <amanuelabiy.as@gmail.com>
2021-09-15 01:10:02 +0300

    todo

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    let dic = {};
+    let tasks = JSON.parse(body);
+    for (let i in tasks) {
+      if (tasks[i].completed) {
+	if (dic[tasks[i].userId] === undefined) {
+	  dic[tasks[i].userId] = 1;
+	} else {
+	  dic[tasks[i].userId]++;
+	}
+      }
+    }
+    console.log(dic);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
+});
