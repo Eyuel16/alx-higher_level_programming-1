9188b9271ee41ba2320c090fce08f56f137793e0
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-09-15 01:41:52 +0300

    fixed semistandard error

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
-
-const url = 'http://swapi.co/api/films/' + process.argv[2];
 const request = require('request');
-
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    body = JSON.parse(body);
-    console.log(body.title);
-  } else {
-    console.log('Erorr Code:' + response.statusCode);
+const id = process.argv[2];
+request('http://swapi.co/api/films/' + id + '/', function (error, response, body) {
+  if (error == null) {
+    const json = JSON.parse(body);
+    console.log(json.title);
   }
 });
