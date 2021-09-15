b3029cfd70fac424d94187a0038bc8c5caa2ff31
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-09-15 23:56:36 +0300

    fixed request

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const url = 'http://swapi.co/api/films/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 const request = require('request');
 
 request(url, function (err, response, body) {
