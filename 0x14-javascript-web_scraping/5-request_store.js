ca1719cb6d938aa5a5f38c7e0c5f443aed39ffe6
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-09-15 23:53:24 +0300

    file writing exception handled

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,13 +2,13 @@
 
 const fs = require('fs');
 const request = require('request');
-filename = process.argv[3];
+const filename = process.argv[3];
 
 request(process.argv[2], function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
-    fs.writeFile(filename, body, 'utf8', function(err, response, body) {
+    fs.writeFile(filename, body, 'utf8', function (err, response, body) {
       if (err) {
         console.log(err);
       }
