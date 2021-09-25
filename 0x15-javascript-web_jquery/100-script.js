1445b40ecba88e0a6592c220c8c32bc4f093ecbd
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-09-26 00:12:16 +0300

    works when added on head

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+(function () {
+    document.addEventListener('DOMContentLoaded', function () {
+        const myheader = document.querySelector('header');
+        myheader.style.color = '#FF0000';
+    })
+}) ();
