c2a55d7651477b1527d30774a2031e78aa50bcff
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-09-26 00:11:27 +0300

    fetch data and append to list

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,6 @@
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function(data, status) {
+    //console.log(data.results);
+    $.each(data.results, function( index, element) {
+        $('#list_movies').append('<li>'+element.title+'</li');
+    });
+});
